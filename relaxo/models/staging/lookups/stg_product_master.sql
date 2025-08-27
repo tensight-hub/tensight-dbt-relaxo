aa3ae2506eaa55ff6446c706836b10cd6c128d0c
Megha Jain <megha@tenovia.com>
2025-08-27 21:10:09 +0530

    initial commit

--- relaxo/models/staging/lookups/stg_product_master.sql
+++ relaxo/models/staging/lookups/stg_product_master.sql
@@ -13,12 +13,12 @@ renamed as (
        "relaxo sku" as relaxo_sku,
         sku as brand_sku_id,
         ean as brand_ean,
-        channel id (asin/fsn) as channel_sku_id,	
+       "channel id (asin/fsn)" as channel_sku_id,	
         category as sku_category,
-        sub category as sku_sub_category,
+        "sub category" as sku_sub_category,
         size as sku_size,
         gender as sku_gender,
-        sub-brand as sub_brand
+        "sub-brand" as sub_brand
 
         
     from source)

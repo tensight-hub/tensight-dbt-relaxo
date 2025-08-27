baee92a6e0ec2d86d6db4e3b51ef68f9171148e4
Megha Jain <megha@tenovia.com>
2025-08-27 20:40:28 +0530

    initial commit

---
+++ relaxo/models/staging/lookups/stg_product_master.sql
@@ -0,0 +1,28 @@
+with
+
+source as (
+
+    select * from {{ source('lookups', 'product_master') }}
+
+),
+
+renamed as (
+
+    select  
+       channel,
+       "relaxo sku" as relaxo_sku,
+        sku as brand_sku_id,
+        ean as brand_ean,
+        channel id (asin/fsn) as channel_sku_id,	
+        category as sku_category,
+        sub category as sku_sub_category,
+        size as sku_size,
+        gender as sku_gender,
+        sub-brand as sub_brand
+
+        
+    from source)
+
+select * from renamed
+
+

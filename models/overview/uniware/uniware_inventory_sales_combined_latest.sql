9e60fae3265a22ac8d6e6fb708522fd021b5128a
Megha Jain <megha@tenovia.com>
2025-10-22 10:51:36 +0530

    code clean up

--- models/overview/uniware/uniware_inventory_sales_combined_latest.sql
+++
@@ -1,8 +0,0 @@
-select latest_inv.*, 
-COALESCE(drr_calc.drr_30d,0) as quantity_30d_drr,
-cast(latest_inv.total/drr_calc.drr_30d as int) as doh_30d
-from {{ ref('uniware_inventory_segment_latest') }} latest_inv 
-left join 
-{{ ref('uniware_sales_drr_calc') }} drr_calc
-on latest_inv.facility_code = drr_calc.facility_code
-and latest_inv.sku_code = drr_calc.sku_id
--- models/overview/uniware/uniware_sales_drr_calc.sql
+++
@@ -1,9 +0,0 @@
-select 
-facility_code,
-sku_id,
-cast(sum(order_quantity) as double)/30 as drr_30d
-from {{ ref('uniware_sales_facility_date') }} 
-where invoice_created between 
-CAST(date_add('day', -30, current_timestamp) AS date)
-AND CAST(date_add('day',  -1, current_timestamp) AS date)
-group by 1,2

f26b53c77f0cb3831b0fc4417b47032049b80eb3
Manideep-g23 <manideep@tenovia.com>
2026-01-29 14:57:55 +0530

    Update int_uniware_sales_sku_wise_rating

--- models/intermediate/uniware/int_uniware_sales_sku_wise_rating.sql
+++ models/intermediate/uniware/int_uniware_sales_sku_wise_rating.sql
@@ -97,7 +97,7 @@ group by 1,2,3,4,5,6,7,8,9,10,
 21,22,23,24,25,26,27,28,29,30,
 31,32,33,34,35,36,37,38,39,40,
 41,42,43,44,45,46,47,48,49,50,
-51,52)
+51,52,53)
 where lower(master_mapping_channel_name) in ('ajio','myntra','flipkart','amazon')
 group by 1,2
 ) uni_sales

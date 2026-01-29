6947381b3707c4611174c0ca62581a090ea4afa9
Manideep-g23 <manideep@tenovia.com>
2026-01-29 14:57:01 +0530

    Update inventory_sales_combined

--- models/overview/uniware/inventory_sales_combined.sql
+++ models/overview/uniware/inventory_sales_combined.sql
@@ -67,7 +67,7 @@ SUM(
                21,22,23,24,25,26,27,28,29,30,
                31,32,33,34,35,36,37,38,39,40,
                41,42,43,44,45,46,47,48,49,50,
-               51,52
+               51,52,53
     ) so
     ON ib.sku_code = so.item_sku_code
     AND ib.facility_code = so.facility_code

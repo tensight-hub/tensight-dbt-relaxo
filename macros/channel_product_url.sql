599c43eeb1c95a37a310b37244285090136b4cec
Manideep-g23 <manideep@tenovia.com>
2026-01-29 14:23:30 +0530

    Update channel_product_url

--- macros/channel_product_url.sql
+++ macros/channel_product_url.sql
@@ -1,6 +1,3 @@
--- {% macro channel_product_url(source_col, url_col, platform, alias) %}
---     MAX(CASE WHEN {{ source_col }} = '{{ platform }}' THEN {{ url_col }} END) AS {{ alias }}
--- {% endmacro %}
 {% macro channel_product_url(source_col, url_col, platform, alias) %}
     MAX(CASE WHEN LOWER({{ source_col }}) = LOWER('{{ platform }}')
             THEN {{ url_col }}
